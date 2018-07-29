eac59d3d3bc87184ecf449cd2cb46ffde5b079a5
Julia Sakamoto <sakamoju@sheridancollege.ca>
2018-07-29 18:44:23 -0400

    Adding files.

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const parseurl = require('parseurl')
+const bodyParser = require('body-parser')
+const path = require('path')
+const expressValidator = require('express-validator')
+const mongoose = require('mongoose')
+const Todo = require('./models/todo-model.js')
+const app = express()
+const url = 'mongodb://user:passw0rd@ds023303.mlab.com:23303/todolist'
+
+
+//Root directory
+app.get('/', function(req, res) {
+  res.json('success')
+})
+
+//Get all
+app.get('/api/todos', function(req, res) {
+  Todo.find({}).then((todoItem) => {
+    res.json(todoItem)
+  })
+})
+
+//Post new signature
+app.post('/api/todos', function(req, res) {
+  Todo.create({
+    status: req.body.Status,
+    title: req.body.Title,
+    date: req.body.Date
+  }).then(todoItem => {
+    res.json(todoItem)
+  })
+})
+
+//Mongoose connect
+mongoose.connect(url, function(err, db) {
+  if (err)
+    console.log('Unable to connect. Error: ', err)
+  else
+    console.log('Connection established with ', url)
+})
+
+const port = process.env.PORT || 3000
+app.listen(port)
