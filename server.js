533e5c76c1cd507733d8133e71b8a02694686577
Julia Sakamoto <sakamoju@sheridancollege.ca>
2018-07-30 19:27:12 -0400

    Changing server.js again

--- server.js
+++ server.js
@@ -29,12 +29,7 @@ app.get('/api/todos', function (req, res) {
 
 //Post new signature
 app.post('/api/todos', function (req, res) {
-  console.log(req.body)
-  
   Todo.create({ title: req.body.Title })
-    .then((todoItem) => {
-      res.json(todoItem)
-    })
 })
 
 //Mongoose connect
